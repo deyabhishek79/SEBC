312741a26ac122c40540584f8071922e07bcf47e
Abhishek Dey <abhishek.dey@wipro.com>
2017-10-11 17:01:54 -0400

    Added Hive Curl commands and CDH Upgrade Screenshot

---
+++ enterprise/labs/4_hive_restart.sh
@@ -0,0 +1,3 @@
+curl -u 'deyabhishek79:cloudera' -X POST "http://52.90.50.48:7180/api/v12/clusters/deyabhishek79/services/hive/commands/stop"
+curl -u 'deyabhishek79:cloudera' -X POST "http://52.90.50.48:7180/api/v12/clusters/deyabhishek79/services/hive/commands/start"
+curl -u 'deyabhishek79:cloudera' -X GET "http://52.90.50.48:7180/api/v12/clusters/deyabhishek79/services/hive"
